1e5a1dd43cda3659520e235293c52b1bc795ea0a
alu0100605983 <alu0100605983@ull.edu.es>
2014-09-26 09:51:54 +0100

    Update configure.rb

--- configure.rb
+++ configure.rb
@@ -1,5 +1,4 @@
-def my_twitter_client
-  Twitter::REST::Client.new do |config|
+Twitter.configure do |config|
   config.consumer_key       = 'JP5F6kyfnlqzuKNUecwN0CCRE'
   config.consumer_secret    = 'mARPL4gKaPD37MQx43EndVf7bVsx3Nbf9ZLLiyzHlQMDTZuH8h'
   config.oauth_token        = "550372508-BZYAfikNqwxldFEzF7acHq887Vl5485aAF7BIMYI"
