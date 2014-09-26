92d67e329fc00f0bb5cfd7642bc6d209d40b356f
alu0100605983 <alu0100605983@ull.edu.es>
2014-09-26 08:59:17 +0100

    Delete configure2.rb

--- configure2.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key       = 'JP5F6kyfnlqzuKNUecwN0CCRE'
-  config.consumer_secret    = 'mARPL4gKaPD37MQx43EndVf7bVsx3Nbf9ZLLiyzHlQMDTZuH8h'
-  config.oauth_token        = "550372508-BZYAfikNqwxldFEzF7acHq887Vl5485aAF7BIMYI"
-  config.oauth_token_secret = "ncJMsNTHAO6pne8ewk1O6ttOoVy7mxXNG8pWe0EOJ7PVq"
-end
